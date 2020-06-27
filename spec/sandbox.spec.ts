c292c1b968986af346a6ff2fb2b5637ea97d1ea3
Girma Nigusse <xgirma@users.noreply.github.com>
2020-06-27 07:57:02 -0400

    Update sandbox.spec.ts

--- spec/sandbox.spec.ts
+++ spec/sandbox.spec.ts
@@ -3,12 +3,10 @@ import { should } from "chai";
 
 should();
 
-fixture`Sandbox`.page`https://e2e-boilerplate.github.io/sandbox/`;
+fixture`Sandbox`.page`https://www.google.com`;
 
 test("should be on Sandbox", async (t) => {
   const title = await Selector("title").innerText;
-  const header = await Selector("h1").innerText;
 
-  title.should.eql("Sandbox");
-  header.should.eql("Sandbox");
+  title.should.eql("Google");
 });
